e8093e755481a72404be8272a1fb769d94ca0419
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2022-09-13 11:31:53 -0700

    message

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * _islower - function to check for lowercase character
@@ -13,5 +13,5 @@ int _islower(int c)
 		return (1);
 	}
 	else
-		return (0);
+	return (0);
 }
